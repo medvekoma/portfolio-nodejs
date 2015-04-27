42b065e14e7673b4a0f9d0c0f9da5f9f2a381440
Attila Szucs <attila_szucs@epam.com>
2015-04-27 16:00:04 +0200

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('portfotolioNodejsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
