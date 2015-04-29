5422bf60d300fe3893c68f5135ff85d155ff42c5
Attila Szucs <attila_szucs@epam.com>
2015-04-29 09:50:05 +0200

    node-debug

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/portfotolionodejs-dev'
+    uri: 'mongodb://localhost/portfotolio-dev'
   },
 
   seedDB: true
